6d3674887f708f2c7101edc41b49dfb66e146304
TheBoy-WhoCode <jiten.pateljp2001@gmail.com>
2022-08-27 17:25:42 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Instagram Clone',
+      theme: ThemeData.dark()
+          .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+      home: Scaffold(),
+    );
+  }
+}
---
+++ lib/responsive/responsive_layout_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'package:instagram_clone/utils/dimensions.dart';
+
+class ResponseLayoutScreen extends StatelessWidget {
+  final Widget webScreenLayout;
+  final Widget mobileScreenLayout;
+  const ResponseLayoutScreen({
+    Key? key,
+    required this.webScreenLayout,
+    required this.mobileScreenLayout,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth > webScreenSize) {
+        return webScreenLayout;
+      }
+      return mobileScreenLayout;
+    });
+  }
+}
